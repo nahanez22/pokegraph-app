b904ed012231d6073dda22210531b27b27b62d7a
Nahum Añez <nahumatencio@gmail.com>
2023-01-27 22:37:07 -0500

    Making the data request to the pokeapi

--- pokegraph-app/src/components/Main.tsx
+++ pokegraph-app/src/components/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 interface PokemonData {
@@ -10,7 +10,27 @@ interface PokemonData {
 const PoKemonGraph: React.FC = () => {
   const [pokemons, setPokemons] = useState<PokemonData[]>([]);
 
-  return <div>Hola</div>;
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=151"
+      );
+      setPokemons(
+        response.data.results.map((pokemon: any) => ({
+          name: pokemon.name,
+          types: pokemon.types.map((type: any) => type.type.name),
+          id: pokemon.id,
+        }))
+      );
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {pokemons.length > 0 ? <Graph pokemons={pokemons} /> : <p>Loading...</p>}
+    </div>
+  );
 };
 
 export default PoKemonGraph;
