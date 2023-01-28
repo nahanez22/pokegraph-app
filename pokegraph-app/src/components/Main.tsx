c7919e57429dc852d35e985f9499e881a0f5256e
Nahum Añez <nahumatencio@gmail.com>
2023-01-27 21:59:15 -0500

    Creating components

---
+++ pokegraph-app/src/components/Main.tsx
@@ -0,0 +1,11 @@
+interface PokemonData {
+  name: string;
+  types: string[];
+  id: number;
+}
+
+const Main = () => {
+  return <p>Hola</p>;
+};
+
+export default Main;
--- pokegraph-app/src/pages/index.tsx
+++ pokegraph-app/src/pages/index.tsx
@@ -1,11 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+import Main from "../components/Main";
 
 export default function Home() {
-  return (
-    <>
-        <p>hola</p>
-    </>
-  )
+  return <Main />;
 }
